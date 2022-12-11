b5b7ee056a04153197e951b65c45c19c999bf6ab
Dave Feucht <poetas@gmail.com>
2022-12-11 13:17:11 +0100

    Adds day4 exercises

---
+++ day4/exercise2.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const data = fs.readFileSync('./data.txt', 'utf8');
+
+const convertRangeToArray = (begin, end) => {
+    const returnArray = [];
+    for (let i = parseInt(begin); i <= parseInt(end); ++i) {
+        returnArray.push(i);
+    }
+
+    return returnArray;
+}
+
+let containedRanges = 0;
+
+// Split data into rows
+const pairs = data.split('\n')
+    // Convert each row into a sub-array representing a 'round'
+    .map(row => {
+        return row.split(',').map(pair => convertRangeToArray(...pair.split('-')));
+    });
+
+pairs.forEach(pair => {
+    if (
+        pair[0].some(element => pair[1].includes(element)) ||
+        pair[1].some(element => pair[0].includes(element))
+    ) {
+        ++containedRanges;
+    }
+});
+
+console.log(containedRanges);
